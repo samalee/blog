8e1de9dd72df6ecbcb081ec21a2748b0ffd5fd0f
Samantha Malay <samaleemalay@gmail.com>
2019-08-26 20:58:14 -0600

    Started Day 1 of rails

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def Pages
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/about, to: static_page_about'
+  resources :pages
+  # get
+  # post
+  # put
+  # delete
+  # https
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
